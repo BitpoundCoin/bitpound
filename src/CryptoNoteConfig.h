6b312d5c6b327186a7c21fda7a78d0f4931d7f61
Bitpound <mox_egy@yahoo.com>
2018-02-16 20:08:27 +0200

    Money Supply Updated

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -24,7 +24,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(9900000000000000000);
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(100000000000000000);
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "bitpound";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000197968cbb8fcb08029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012a6702c2779a07039fb8ec7d01d885db3529c8d54bd2640437d349ebe3d7bdca";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -85,9 +85,9 @@ const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by def
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int      P2P_DEFAULT_PORT                              = 19575;
+const int      P2P_DEFAULT_PORT                              = 39575;
 //TODO This port will be used by the daemon to interact with simlewallet
-const int      RPC_DEFAULT_PORT                              = 19576;
+const int      RPC_DEFAULT_PORT                              = 39576;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -106,8 +106,13 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "18.221.146.19:19575",
-  "18.220.112.177:19575"
+  "node01.bitpound.net:39575",
+  "node02.bitpound.net:39575",
+  "node03.bitpound.net:39575",
+  "node04.bitpound.net:39575",
+  "node05.bitpound.net:39575",
+  "node06.bitpound.net:39575",
+  "node07.bitpound.net:39575"
 };
 
 struct CheckpointData {
